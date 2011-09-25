1b91862eb27a3e20c18cd147818508924296cea5
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2011-09-25 03:19:37 +0000

    New store process was being implemented.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@998 89dd94fa-70ca-40ae-8747-2094edff379b

--- v0_1_0/trunk/src/store.c
+++ v0_1_0/trunk/src/store.c
@@ -19,6 +19,565 @@ static int store_bottom = 0;
 static int xtra_stock = 0;
 static s16b old_town_num = 0;
 static s16b inner_town_num = 0;
+
+/*** Initialize others ***/
+
+/*
+ * Hack -- Objects sold in the stores -- by tval/sval pair.
+ */
+static byte store_table[MAX_STORES][STORE_CHOICES][2] =
+{
+	{
+		/* General Store */
+
+		{ TV_FOOD, SV_FOOD_RATION },
+		{ TV_FOOD, SV_FOOD_RATION },
+		{ TV_FOOD, SV_FOOD_RATION },
+		{ TV_FOOD, SV_FOOD_RATION },
+
+		{ TV_FOOD, SV_FOOD_RATION },
+		{ TV_FOOD, SV_FOOD_BISCUIT },
+		{ TV_FOOD, SV_FOOD_JERKY },
+		{ TV_FOOD, SV_FOOD_JERKY },
+
+		{ TV_FOOD, SV_FOOD_PINT_OF_WINE },
+		{ TV_FOOD, SV_FOOD_PINT_OF_ALE },
+		{ TV_LITE, SV_LITE_TORCH },
+		{ TV_LITE, SV_LITE_TORCH },
+
+		{ TV_LITE, SV_LITE_TORCH },
+		{ TV_LITE, SV_LITE_TORCH },
+		{ TV_LITE, SV_LITE_LANTERN },
+		{ TV_LITE, SV_LITE_LANTERN },
+
+		{ TV_FLASK, 0 },
+		{ TV_FLASK, 0 },
+		{ TV_FLASK, 0 },
+		{ TV_FLASK, 0 },
+
+		{ TV_FLASK, 0 },
+		{ TV_FLASK, 0 },
+		{ TV_SPIKE, 0 },
+		{ TV_SPIKE, 0 },
+
+		{ TV_SHOT, SV_AMMO_NORMAL },
+		{ TV_ARROW, SV_AMMO_NORMAL },
+		{ TV_BOLT, SV_AMMO_NORMAL },
+		{ TV_DIGGING, SV_SHOVEL },
+
+		{ TV_DIGGING, SV_PICK },
+		{ TV_CLOAK, SV_CLOAK },
+		{ TV_CLOAK, SV_CLOAK },
+		{ TV_CLOAK, SV_FUR_CLOAK },
+
+		{ TV_FOOD, SV_FOOD_RATION },
+		{ TV_FOOD, SV_FOOD_RATION },
+		{ TV_FOOD, SV_FOOD_RATION },
+		{ TV_FOOD, SV_FOOD_RATION },
+
+		{ TV_POTION, SV_POTION_WATER },
+		{ TV_POTION, SV_POTION_WATER },
+		{ TV_LITE, SV_LITE_LANTERN },
+		{ TV_LITE, SV_LITE_LANTERN },
+
+		{ TV_FOOD, SV_FOOD_WAYBREAD },
+		{ TV_FOOD, SV_FOOD_WAYBREAD },
+		{ TV_CAPTURE, 0 },
+		{ TV_FIGURINE, 0 },
+
+		{ TV_SHOT, SV_AMMO_NORMAL },
+		{ TV_ARROW, SV_AMMO_NORMAL },
+		{ TV_BOLT, SV_AMMO_NORMAL },
+		{ TV_DIGGING, SV_SHOVEL }
+	},
+
+	{
+		/* Armoury */
+
+		{ TV_BOOTS, SV_PAIR_OF_SOFT_LEATHER_BOOTS },
+		{ TV_BOOTS, SV_PAIR_OF_SOFT_LEATHER_BOOTS },
+		{ TV_BOOTS, SV_PAIR_OF_HARD_LEATHER_BOOTS },
+		{ TV_BOOTS, SV_PAIR_OF_HARD_LEATHER_BOOTS },
+
+		{ TV_HELM, SV_HARD_LEATHER_CAP },
+		{ TV_HELM, SV_HARD_LEATHER_CAP },
+		{ TV_HELM, SV_METAL_CAP },
+		{ TV_HELM, SV_IRON_HELM },
+
+		{ TV_SOFT_ARMOR, SV_ROBE },
+		{ TV_SOFT_ARMOR, SV_ROBE },
+		{ TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR },
+		{ TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR },
+
+		{ TV_SOFT_ARMOR, SV_HARD_LEATHER_ARMOR },
+		{ TV_SOFT_ARMOR, SV_HARD_LEATHER_ARMOR },
+		{ TV_SOFT_ARMOR, SV_HARD_STUDDED_LEATHER },
+		{ TV_SOFT_ARMOR, SV_HARD_STUDDED_LEATHER },
+
+		{ TV_SOFT_ARMOR, SV_RHINO_HIDE_ARMOR },
+		{ TV_SOFT_ARMOR, SV_LEATHER_SCALE_MAIL },
+		{ TV_HARD_ARMOR, SV_METAL_SCALE_MAIL },
+		{ TV_HARD_ARMOR, SV_CHAIN_MAIL },
+
+		{ TV_HARD_ARMOR, SV_DOUBLE_RING_MAIL },
+		{ TV_HARD_ARMOR, SV_AUGMENTED_CHAIN_MAIL },
+		{ TV_HARD_ARMOR, SV_BAR_CHAIN_MAIL },
+		{ TV_HARD_ARMOR, SV_DOUBLE_CHAIN_MAIL },
+
+		{ TV_HARD_ARMOR, SV_METAL_BRIGANDINE_ARMOUR },
+		{ TV_HARD_ARMOR, SV_SPLINT_MAIL },
+		{ TV_GLOVES, SV_SET_OF_LEATHER_GLOVES },
+		{ TV_GLOVES, SV_SET_OF_LEATHER_GLOVES },
+
+		{ TV_GLOVES, SV_SET_OF_GAUNTLETS },
+		{ TV_SHIELD, SV_SMALL_LEATHER_SHIELD },
+		{ TV_SHIELD, SV_LARGE_LEATHER_SHIELD },
+		{ TV_SHIELD, SV_SMALL_METAL_SHIELD },
+
+		{ TV_BOOTS, SV_PAIR_OF_HARD_LEATHER_BOOTS },
+		{ TV_BOOTS, SV_PAIR_OF_HARD_LEATHER_BOOTS },
+		{ TV_HELM, SV_HARD_LEATHER_CAP },
+		{ TV_HELM, SV_HARD_LEATHER_CAP },
+
+		{ TV_SOFT_ARMOR, SV_ROBE },
+		{ TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR },
+		{ TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR },
+		{ TV_SOFT_ARMOR, SV_HARD_LEATHER_ARMOR },
+
+		{ TV_SOFT_ARMOR, SV_LEATHER_JACK },
+		{ TV_HARD_ARMOR, SV_METAL_SCALE_MAIL },
+		{ TV_HARD_ARMOR, SV_CHAIN_MAIL },
+		{ TV_HARD_ARMOR, SV_CHAIN_MAIL },
+
+		{ TV_GLOVES, SV_SET_OF_LEATHER_GLOVES },
+		{ TV_GLOVES, SV_SET_OF_GAUNTLETS },
+		{ TV_SHIELD, SV_SMALL_LEATHER_SHIELD },
+		{ TV_SHIELD, SV_SMALL_LEATHER_SHIELD }
+	},
+
+	{
+		/* Weaponsmith */
+
+		{ TV_SWORD, SV_DAGGER },
+		{ TV_SWORD, SV_MAIN_GAUCHE },
+		{ TV_SWORD, SV_RAPIER },
+		{ TV_SWORD, SV_SMALL_SWORD },
+
+		{ TV_SWORD, SV_SHORT_SWORD },
+		{ TV_SWORD, SV_SABRE },
+		{ TV_SWORD, SV_CUTLASS },
+		{ TV_SWORD, SV_TULWAR },
+
+		{ TV_SWORD, SV_BROAD_SWORD },
+		{ TV_SWORD, SV_LONG_SWORD },
+		{ TV_SWORD, SV_SCIMITAR },
+		{ TV_SWORD, SV_KATANA },
+
+		{ TV_SWORD, SV_BASTARD_SWORD },
+		{ TV_POLEARM, SV_SPEAR },
+		{ TV_POLEARM, SV_AWL_PIKE },
+		{ TV_POLEARM, SV_TRIDENT },
+
+		{ TV_POLEARM, SV_PIKE },
+		{ TV_POLEARM, SV_BEAKED_AXE },
+		{ TV_POLEARM, SV_BROAD_AXE },
+		{ TV_POLEARM, SV_LANCE },
+
+		{ TV_POLEARM, SV_BATTLE_AXE },
+		{ TV_POLEARM, SV_HATCHET },
+		{ TV_BOW, SV_SLING },
+		{ TV_BOW, SV_SHORT_BOW },
+
+		{ TV_BOW, SV_LIGHT_XBOW },
+		{ TV_SHOT, SV_AMMO_NORMAL },
+		{ TV_SHOT, SV_AMMO_NORMAL },
+		{ TV_ARROW, SV_AMMO_NORMAL },
+
+		{ TV_ARROW, SV_AMMO_NORMAL },
+		{ TV_BOLT, SV_AMMO_NORMAL },
+		{ TV_BOLT, SV_AMMO_NORMAL },
+		{ TV_BOW, SV_LIGHT_XBOW },
+
+		{ TV_ARROW, SV_AMMO_NORMAL },
+		{ TV_BOLT, SV_AMMO_NORMAL },
+		{ TV_BOW, SV_SHORT_BOW },
+		{ TV_BOW, SV_LIGHT_XBOW },
+
+		{ TV_SWORD, SV_DAGGER },
+		{ TV_SWORD, SV_TANTO },
+		{ TV_SWORD, SV_RAPIER },
+		{ TV_SWORD, SV_SMALL_SWORD },
+
+		{ TV_SWORD, SV_SHORT_SWORD },
+		{ TV_SWORD, SV_LONG_SWORD },
+		{ TV_SWORD, SV_SCIMITAR },
+		{ TV_SWORD, SV_BROAD_SWORD },
+
+		{ TV_HISSATSU_BOOK, 0 },
+		{ TV_HISSATSU_BOOK, 0 },
+		{ TV_HISSATSU_BOOK, 1 },
+		{ TV_HISSATSU_BOOK, 1 },
+	},
+
+	{
+		/* Temple */
+
+		{ TV_HAFTED, SV_NUNCHAKU },
+		{ TV_HAFTED, SV_QUARTERSTAFF },
+		{ TV_HAFTED, SV_MACE },
+		{ TV_HAFTED, SV_BO_STAFF },
+
+		{ TV_HAFTED, SV_WAR_HAMMER },
+		{ TV_HAFTED, SV_WAR_HAMMER },
+		{ TV_HAFTED, SV_MORNING_STAR },
+		{ TV_HAFTED, SV_FLAIL },
+
+		{ TV_HAFTED, SV_LEAD_FILLED_MACE },
+		{ TV_SCROLL, SV_SCROLL_REMOVE_CURSE },
+		{ TV_SCROLL, SV_SCROLL_BLESSING },
+		{ TV_SCROLL, SV_SCROLL_HOLY_CHANT },
+
+		{ TV_POTION, SV_POTION_HEROISM },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+
+		{ TV_POTION, SV_POTION_CURE_LIGHT },
+		{ TV_POTION, SV_POTION_CURE_SERIOUS },
+		{ TV_POTION, SV_POTION_CURE_SERIOUS },
+		{ TV_POTION, SV_POTION_CURE_CRITICAL },
+
+		{ TV_POTION, SV_POTION_CURE_CRITICAL },
+		{ TV_POTION, SV_POTION_RESTORE_EXP },
+		{ TV_POTION, SV_POTION_RESTORE_EXP },
+		{ TV_POTION, SV_POTION_RESTORE_EXP },
+
+		{ TV_LIFE_BOOK, 0 },
+		{ TV_LIFE_BOOK, 0 },
+		{ TV_LIFE_BOOK, 1 },
+		{ TV_LIFE_BOOK, 1 },
+
+		{ TV_CRUSADE_BOOK, 0 },
+		{ TV_CRUSADE_BOOK, 0 },
+		{ TV_CRUSADE_BOOK, 1 },
+		{ TV_CRUSADE_BOOK, 1 },
+
+		{ TV_HAFTED, SV_WHIP },
+		{ TV_HAFTED, SV_MACE },
+		{ TV_HAFTED, SV_BALL_AND_CHAIN },
+		{ TV_HAFTED, SV_WAR_HAMMER },
+
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_POTION, SV_POTION_CURE_CRITICAL },
+
+		{ TV_POTION, SV_POTION_CURE_CRITICAL },
+		{ TV_POTION, SV_POTION_RESTORE_EXP },
+
+		{ TV_FIGURINE, 0 },
+		{ TV_STATUE, SV_ANY },
+
+		{ TV_SCROLL, SV_SCROLL_REMOVE_CURSE },
+		{ TV_SCROLL, SV_SCROLL_REMOVE_CURSE },
+		{ TV_SCROLL, SV_SCROLL_STAR_REMOVE_CURSE },
+		{ TV_SCROLL, SV_SCROLL_STAR_REMOVE_CURSE }
+	},
+
+	{
+		/* Alchemy shop */
+
+		{ TV_SCROLL, SV_SCROLL_ENCHANT_WEAPON_TO_HIT },
+		{ TV_SCROLL, SV_SCROLL_ENCHANT_WEAPON_TO_DAM },
+		{ TV_SCROLL, SV_SCROLL_ENCHANT_ARMOR },
+		{ TV_SCROLL, SV_SCROLL_IDENTIFY },
+
+		{ TV_SCROLL, SV_SCROLL_IDENTIFY },
+		{ TV_SCROLL, SV_SCROLL_IDENTIFY },
+		{ TV_SCROLL, SV_SCROLL_IDENTIFY },
+		{ TV_SCROLL, SV_SCROLL_LIGHT },
+
+		{ TV_SCROLL, SV_SCROLL_PHASE_DOOR },
+		{ TV_SCROLL, SV_SCROLL_PHASE_DOOR },
+		{ TV_SCROLL, SV_SCROLL_TELEPORT },
+		{ TV_SCROLL, SV_SCROLL_MONSTER_CONFUSION },
+
+		{ TV_SCROLL, SV_SCROLL_MAPPING },
+		{ TV_SCROLL, SV_SCROLL_DETECT_GOLD },
+		{ TV_SCROLL, SV_SCROLL_DETECT_ITEM },
+		{ TV_SCROLL, SV_SCROLL_DETECT_TRAP },
+
+		{ TV_SCROLL, SV_SCROLL_DETECT_INVIS },
+		{ TV_SCROLL, SV_SCROLL_RECHARGING },
+		{ TV_SCROLL, SV_SCROLL_TELEPORT },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_SCROLL, SV_SCROLL_WORD_OF_RECALL },
+		{ TV_SCROLL, SV_SCROLL_TELEPORT },
+
+		{ TV_SCROLL, SV_SCROLL_TELEPORT },
+		{ TV_POTION, SV_POTION_RES_STR },
+		{ TV_POTION, SV_POTION_RES_INT },
+		{ TV_POTION, SV_POTION_RES_WIS },
+
+		{ TV_POTION, SV_POTION_RES_DEX },
+		{ TV_POTION, SV_POTION_RES_CON },
+		{ TV_POTION, SV_POTION_RES_CHR },
+		{ TV_SCROLL, SV_SCROLL_IDENTIFY },
+
+		{ TV_SCROLL, SV_SCROLL_IDENTIFY },
+		{ TV_SCROLL, SV_SCROLL_STAR_IDENTIFY },  /* Yep, occasionally! */
+		{ TV_SCROLL, SV_SCROLL_STAR_IDENTIFY },
+		{ TV_SCROLL, SV_SCROLL_LIGHT },
+
+		{ TV_POTION, SV_POTION_RES_STR },
+		{ TV_POTION, SV_POTION_RES_INT },
+		{ TV_POTION, SV_POTION_RES_WIS },
+		{ TV_POTION, SV_POTION_RES_DEX },
+
+		{ TV_POTION, SV_POTION_RES_CON },
+		{ TV_POTION, SV_POTION_RES_CHR },
+		{ TV_SCROLL, SV_SCROLL_ENCHANT_ARMOR },
+		{ TV_SCROLL, SV_SCROLL_ENCHANT_ARMOR },
+
+		{ TV_SCROLL, SV_SCROLL_RECHARGING },
+		{ TV_SCROLL, SV_SCROLL_PHASE_DOOR },
+		{ TV_SCROLL, SV_SCROLL_ENCHANT_WEAPON_TO_HIT },
+		{ TV_SCROLL, SV_SCROLL_ENCHANT_WEAPON_TO_DAM },
+
+	},
+
+	{
+		/* Magic-User store */
+
+		{ TV_RING, SV_RING_PROTECTION },
+		{ TV_RING, SV_RING_LEVITATION_FALL },
+		{ TV_RING, SV_RING_PROTECTION },
+		{ TV_RING, SV_RING_RESIST_FIRE },
+
+		{ TV_RING, SV_RING_RESIST_COLD },
+		{ TV_AMULET, SV_AMULET_CHARISMA },
+		{ TV_RING, SV_RING_WARNING },
+		{ TV_AMULET, SV_AMULET_RESIST_ACID },
+
+		{ TV_AMULET, SV_AMULET_SEARCHING },
+		{ TV_WAND, SV_WAND_SLOW_MONSTER },
+		{ TV_WAND, SV_WAND_CONFUSE_MONSTER },
+		{ TV_WAND, SV_WAND_SLEEP_MONSTER },
+
+		{ TV_WAND, SV_WAND_MAGIC_MISSILE },
+		{ TV_WAND, SV_WAND_STINKING_CLOUD },
+		{ TV_WAND, SV_WAND_WONDER },
+		{ TV_WAND, SV_WAND_DISARMING },
+
+		{ TV_STAFF, SV_STAFF_LITE },
+		{ TV_STAFF, SV_STAFF_MAPPING },
+		{ TV_STAFF, SV_STAFF_DETECT_TRAP },
+		{ TV_STAFF, SV_STAFF_DETECT_DOOR },
+
+		{ TV_STAFF, SV_STAFF_DETECT_GOLD },
+		{ TV_STAFF, SV_STAFF_DETECT_ITEM },
+		{ TV_STAFF, SV_STAFF_DETECT_INVIS },
+		{ TV_STAFF, SV_STAFF_DETECT_EVIL },
+
+		{ TV_STAFF, SV_STAFF_TELEPORTATION },
+		{ TV_STAFF, SV_STAFF_TELEPORTATION },
+		{ TV_STAFF, SV_STAFF_TELEPORTATION },
+		{ TV_STAFF, SV_STAFF_TELEPORTATION },
+
+		{ TV_STAFF, SV_STAFF_IDENTIFY },
+		{ TV_STAFF, SV_STAFF_IDENTIFY },
+		{ TV_STAFF, SV_STAFF_IDENTIFY },
+
+		{ TV_STAFF, SV_STAFF_IDENTIFY },
+		{ TV_STAFF, SV_STAFF_REMOVE_CURSE },
+		{ TV_STAFF, SV_STAFF_CURE_LIGHT },
+		{ TV_STAFF, SV_STAFF_PROBING },
+
+		{ TV_FIGURINE, 0 },
+
+		{ TV_SORCERY_BOOK, 0 },
+		{ TV_SORCERY_BOOK, 0 },
+		{ TV_SORCERY_BOOK, 1 },
+		{ TV_SORCERY_BOOK, 1 },
+
+		{ TV_ARCANE_BOOK, 0 },
+		{ TV_ARCANE_BOOK, 0 },
+		{ TV_ARCANE_BOOK, 1 },
+		{ TV_ARCANE_BOOK, 1 },
+
+		{ TV_ARCANE_BOOK, 2 },
+		{ TV_ARCANE_BOOK, 2 },
+		{ TV_ARCANE_BOOK, 3 },
+		{ TV_ARCANE_BOOK, 3 },
+
+	},
+
+	{
+		/* Black Market (unused) */
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 }
+	},
+
+	{
+		/* Home (unused) */
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 }
+	},
+
+	{
+		/* Bookstore */
+		{ TV_SORCERY_BOOK, 0 },
+		{ TV_SORCERY_BOOK, 0 },
+		{ TV_SORCERY_BOOK, 1 },
+		{ TV_SORCERY_BOOK, 1 },
+
+		{ TV_NATURE_BOOK, 0 },
+		{ TV_NATURE_BOOK, 0 },
+		{ TV_NATURE_BOOK, 1 },
+		{ TV_NATURE_BOOK, 1 },
+
+		{ TV_CHAOS_BOOK, 0 },
+		{ TV_CHAOS_BOOK, 0 },
+		{ TV_CHAOS_BOOK, 1 },
+		{ TV_CHAOS_BOOK, 1 },
+
+		{ TV_DEATH_BOOK, 0 },
+		{ TV_DEATH_BOOK, 0 },
+		{ TV_DEATH_BOOK, 1 },
+		{ TV_DEATH_BOOK, 1 },
+
+		{ TV_TRUMP_BOOK, 0 },		/* +16 */
+		{ TV_TRUMP_BOOK, 0 },
+		{ TV_TRUMP_BOOK, 1 },
+		{ TV_TRUMP_BOOK, 1 },
+
+		{ TV_ARCANE_BOOK, 0 },
+		{ TV_ARCANE_BOOK, 1 },
+		{ TV_ARCANE_BOOK, 2 },
+		{ TV_ARCANE_BOOK, 3 },
+
+		{ TV_CRAFT_BOOK, 0 },
+		{ TV_CRAFT_BOOK, 0 },
+		{ TV_CRAFT_BOOK, 1 },
+		{ TV_CRAFT_BOOK, 1 },
+
+		{ TV_DAEMON_BOOK, 0 },
+		{ TV_DAEMON_BOOK, 0 },
+		{ TV_DAEMON_BOOK, 1 },
+		{ TV_DAEMON_BOOK, 1 },
+
+		{ TV_MUSIC_BOOK, 0 },
+		{ TV_MUSIC_BOOK, 0 },
+		{ TV_MUSIC_BOOK, 1 },
+		{ TV_MUSIC_BOOK, 1 },
+
+		{ TV_HEX_BOOK, 0 },
+		{ TV_HEX_BOOK, 0 },
+		{ TV_HEX_BOOK, 1 },
+		{ TV_HEX_BOOK, 1 },
+	},
+
+	{
+		/* Museum (unused) */
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 },
+		{ 0, 0 }
+	}
+};
+
+
 #define RUMOR_CHANCE 8
 
 #define MAX_COMMENT_1	6
@@ -2009,25 +2568,23 @@ static void store_delete(store_type *st_ptr)
  *
  * Should we check for "permission" to have the given item?
  */
-static void store_replacement(store_type *st_ptr)
+static int store_replacement(store_type *st_ptr, int num)
 {
 	int i, tries, level;
 	int size;
+	int suc = 0;
 
 	object_type forge;
 	object_type *q_ptr;
 
-	//TODO:Debug
 
 	/* Paranoia -- no room left */
-	if (st_ptr->stock_num >= st_ptr->stock_size) return;
-
+	if (st_ptr->stock_num >= st_ptr->stock_size) return 0;
 
 	/* Hack -- consider up to four items */
-	for (tries = 0; tries < 4; tries++)
+	for (tries = 0; tries < num; tries++)
 	{
-		/* Black Market */
-		if (is_black_market(st_ptr))
+		if (is_black_market(st_ptr)) // Black Market
 		{
 			/* Pick a level for object/magic */
 			level = rand_range(st_ptr->level / 2, st_ptr->level);
@@ -2038,24 +2595,21 @@ static void store_replacement(store_type *st_ptr)
 			/* Handle failure */
 			if (!i) continue;
 		}
-
-		/* Normal Store */
-		else
+		else // Normal Store
 		{
-			/* Hack -- Pick an item to sell */
+			// Hack -- Pick an item to sell
 			i = st_ptr->table[randint0(st_ptr->table_num)];
 
-			/* Hack -- fake level for apply_magic() */
+			// Hack -- fake level for apply_magic()
 			level = rand_range(1, st_ptr->level);
 		}
-
 
 		/* Get local object */
 		q_ptr = &forge;
 
 		/* Set Standard Item Size */
 
-		/*
+		/* TODO:: Size Set...
 		if(randint0(10) < 9){
 			int s = 0, t = 0;
 			for(j = 0; j < MAX_RACES; j++)
@@ -2088,15 +2642,13 @@ static void store_replacement(store_type *st_ptr)
 
 		object_prep(q_ptr, i, size);
 
-
 		/* Create a new object of the chosen kind */
-
 
 		/* Apply some "low-level" magic (no artifacts) */
 		apply_magic(q_ptr, level, AM_NO_FIXED_ART);
 
 		/* Require valid object */
-		if (!store_will_buy(st_ptr, NULL, q_ptr)) continue;
+		//if (!store_will_buy(st_ptr, NULL, q_ptr)) continue;
 
 		/* Hack -- Charge lite's */
 		if (q_ptr->tval == TV_LITE)
@@ -2104,7 +2656,6 @@ static void store_replacement(store_type *st_ptr)
 			if (q_ptr->sval == SV_LITE_TORCH) q_ptr->xtra4 = FUEL_TORCH / 2;
 			if (q_ptr->sval == SV_LITE_LANTERN) q_ptr->xtra4 = FUEL_LAMP / 2;
 		}
-
 
 		/* The item is "known" */
 		object_known(q_ptr);
@@ -2118,33 +2669,31 @@ static void store_replacement(store_type *st_ptr)
 		/* Prune the black market */
 		if (is_black_market(st_ptr))
 		{
-			/* Hack -- No "crappy" items */
+			// Hack -- No "crappy" items
 			if (black_market_crap(st_ptr, q_ptr)) continue;
 
-			/* Hack -- No "cheap" items */
+			// Hack -- No "cheap" items
 			if (object_value(q_ptr) < 10) continue;
 
-			/* No "worthless" items */
-			/* if (object_value(q_ptr) <= 0) continue; */
 		}
-
-		/* Prune normal stores */
-		else
+		else // Prune normal stores
 		{
-			/* No "worthless" items */
+			// No "worthless" items
 			if (object_value(q_ptr) <= 0) continue;
 		}
-
 
 		/* Mass produce and/or Apply discount */
 		mass_produce(st_ptr, q_ptr);
 
 		/* Attempt to carry the (known) item */
 		(void)store_carry(st_ptr, q_ptr);
+		suc++;
 
 		/* Definitely done */
 		break;
 	}
+
+	return suc;
 }
 
 
@@ -5094,9 +5643,7 @@ void store_process(creature_type *cr_ptr, store_type *st_ptr)
  */
 void store_maint(store_type *st_ptr)
 {
-	int 		j;
-	//TODO
-	int store_num = st_ptr->type;
+	int j;
 
 	/* Ignore home */
 	if (is_home(st_ptr)) return;
@@ -5159,7 +5706,11 @@ void store_maint(store_type *st_ptr)
 	if (j >= st_ptr->stock_size) j = st_ptr->stock_size - 1;
 
 	/* Acquire some new items */
-	while (st_ptr->stock_num < j) store_replacement(st_ptr);
+	if(!store_replacement(st_ptr, j - st_ptr->stock_num) && (j - st_ptr->stock_num > 0))
+	{
+		msg_print("Warning:: Store's Replacement was failed..");
+	}
+	
 }
 
 void move_to_black_market(object_type *o_ptr)
@@ -5202,8 +5753,9 @@ void init_stores(void)
 
 void store_create(store_type *st_ptr, store_pre_type *stp_ptr)
 {
-	int k;
+	int j, k;
 
+	j = st_ptr->type;
 	st_ptr->name = stp_ptr->name;
 
 	st_ptr->type = 0;
@@ -5237,9 +5789,6 @@ void store_create(store_type *st_ptr, store_pre_type *stp_ptr)
 	/* Allocate the stock */
 	C_MAKE(st_ptr->table, st_ptr->table_size, s16b);
 
-
-	// Scan the choices
-	/*
 	for (k = 0; k < STORE_CHOICES; k++)
 	{
 		int k_idx;
@@ -5263,7 +5812,7 @@ void store_create(store_type *st_ptr, store_pre_type *stp_ptr)
 		// Add that item index to the table
 		st_ptr->table[st_ptr->table_num++] = k_idx;
 	}
-	*/
+
 	// Clear any old items
 
 	for (k = 0; k < st_ptr->stock_size; k++)
