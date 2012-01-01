063085afe3e879e81abad9c2102dc02a76bf0975
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-01-01 08:45:24 +0000

    Rename trunk of old version.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@1874 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ v0_0_0/deeangband.sh
@@ -0,0 +1,9 @@
+export ANGBAND_X11_FONT="-*-*-medium-r-normal--16-*-*-*-*-*-iso10646-1"
+export ANGBAND_X11_COLS_0=100
+export ANGBAND_X11_ROWS_0=40
+export LANG="ja_JP.EUC-JP"
+
+./deeangband
+
+
+
