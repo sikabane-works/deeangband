f8ce974d1bfcab2923a0d2e8252be31e6f1eaef6
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-03-19 14:03:04 +0000

    Rename.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@2294 89dd94fa-70ca-40ae-8747-2094edff379b

--- v0_2_0/trunk/deeangband.sh
+++
@@ -1,9 +0,0 @@
-export ANGBAND_X11_FONT="-*-*-medium-r-normal--16-*-*-*-*-*-iso10646-1"
-export ANGBAND_X11_COLS_0=100
-export ANGBAND_X11_ROWS_0=40
-export LANG="ja_JP.EUC-JP"
-
-./deeangband
-
-
-
