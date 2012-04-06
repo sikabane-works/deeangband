c8834e83280c8499a0bdbb61694674ad7fc8d444
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-04-06 11:07:55 +0000

    Fix compact_objects(). Expand object max.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@2501 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/object2.c
+++ source/trunk/src/object2.c
@@ -386,7 +386,7 @@ void compact_objects(int size)
 			}
 
 			/* Nearby objects start out "immune" */
-			if ((cur_dis > 0) && (distance(p_ptr->fy, p_ptr->fx, y, x) < cur_dis)) continue;
+			//if ((cur_dis > 0) && (distance(player_ptr->fy, player_ptr->fx, y, x) < cur_dis)) continue;
 
 			/* Saving throw */
 			chance = 90;
