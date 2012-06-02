a84391d0f3c25eec60f6d8d733dd4c5364ad3bc1
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-06-02 14:47:43 +0000

    Fix generate_floor_world().
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@3275 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/wild.c
+++ source/trunk/src/wild.c
@@ -635,7 +635,7 @@ void generate_floor_world(floor_type *floor_ptr)
 	}
 
 	// Init the wilderness
-	process_dungeon_file(current_floor_ptr, "w_info.txt", 0, 0, max_wild_y, max_wild_x);
+	process_dungeon_file(floor_ptr, "w_info.txt", 0, 0, max_wild_y, max_wild_x);
 
 	// Fill the map
 	for (i = 0; i < max_wild_x; i++)
