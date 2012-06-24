3a6b002d0117ff538ba2ed095bbbdc370b49c50e
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-06-24 00:44:02 +0000

    Fixing display for melee status.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@3449 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/files.c
+++ source/trunk/src/files.c
@@ -1867,8 +1867,12 @@ static void display_player_middle(creature_type *creature_ptr)
 	{
 		if(creature_ptr->can_melee[i])
 		{
-			j = calc_melee_cost(creature_ptr, get_equipped_slot_ptr(creature_ptr, INVEN_SLOT_HAND, i - MELEE_TYPE_WEAPON_1ST));
-			c_put_str(TERM_YELLOW, format("                           %3d", j), 15 + melee_num, 1);
+			object_type *weapon_ptr = get_equipped_slot_ptr(creature_ptr, INVEN_SLOT_HAND, i - MELEE_TYPE_WEAPON_1ST);
+			j = calc_melee_cost(creature_ptr, weapon_ptr);
+			Term_queue_bigchar(15 + melee_num, 1, object_attr(weapon_ptr), object_char(weapon_ptr), 0, 0);
+
+			c_put_str(TERM_WHITE, mention_use(creature_ptr, INVEN_SLOT_HAND, i + 1), 15 + melee_num, 4);
+			c_put_str(TERM_YELLOW, format("%3d", j), 15 + melee_num, 28);
 			melee_num++;
 		}
 	}
