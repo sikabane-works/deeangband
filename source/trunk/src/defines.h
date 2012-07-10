d9594127e77e6288abe450b5b12b3052687ee1a1
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-07-10 00:24:24 +0000

    Fix definiton.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@3677 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/defines.h
+++ source/trunk/src/defines.h
@@ -5184,43 +5184,46 @@ enum SNIPER_TYPE
 	(((USER)->realm1 == REALM_HEX) && ((USER)->magic_num1[0]))
 #define hex_spelling(USER, X) \
 	(((USER)->realm1 == REALM_HEX) && ((USER)->magic_num1[0] & (1L << (X))))
-/* 1st book */
-#define HEX_BLESS             0
-#define HEX_CURE_LIGHT        1
-#define HEX_DEMON_AURA        2
-#define HEX_STINKING_MIST     3
-#define HEX_XTRA_MIGHT        4
-#define HEX_CURSE_WEAPON      5
-#define HEX_DETECT_EVIL       6
-#define HEX_PATIENCE          7
-/* 2nd book */
-#define HEX_ICE_ARMOR         8
-#define HEX_CURE_SERIOUS      9
-#define HEX_INHAIL           10
-#define HEX_VAMP_MIST        11
-#define HEX_RUNESWORD        12
-#define HEX_CONFUSION        13
-#define HEX_BUILDING         14
-#define HEX_ANTI_TELE        15
-/* 3rd book */
-#define HEX_SHOCK_CLOAK      16
-#define HEX_CURE_CRITICAL    17
-#define HEX_RECHARGE         18
-#define HEX_RAISE_DEAD       19
-#define HEX_CURSE_ARMOUR     20
-#define HEX_SHADOW_CLOAK     21
-#define HEX_PAIN_TO_MANA     22
-#define HEX_EYE_FOR_EYE      23
-/* 4th book */
-#define HEX_ANTI_MULTI       24
-#define HEX_RESTORE          25
-#define HEX_DRAIN_CURSE      26
-#define HEX_VAMP_BLADE       27
-#define HEX_stun_creatures    28
-#define HEX_SHADOW_MOVE      29
-#define HEX_ANTI_MAGIC       30
-#define HEX_REVENGE          31
 
+
+enum HEX_SPELL
+{
+	HEX_BLESS,
+	HEX_CURE_LIGHT,
+	HEX_DEMON_AURA,
+	HEX_STINKING_MIST,
+	HEX_XTRA_MIGHT,
+	HEX_CURSE_WEAPON,
+	HEX_DETECT_EVIL,
+	HEX_PATIENCE,
+
+	HEX_ICE_ARMOR,
+	HEX_CURE_SERIOUS,
+	HEX_INHAIL,
+	HEX_VAMP_MIST,
+	HEX_RUNESWORD,
+	HEX_CONFUSION,
+	HEX_BUILDING,
+	HEX_ANTI_TELE,
+
+	HEX_SHOCK_CLOAK,
+	HEX_CURE_CRITICAL,
+	HEX_RECHARGE,
+	HEX_RAISE_DEAD,
+	HEX_CURSE_ARMOUR,
+	HEX_SHADOW_CLOAK,
+	HEX_PAIN_TO_MANA,
+	HEX_EYE_FOR_EYE,
+
+	HEX_ANTI_MULTI,
+	HEX_RESTORE,
+	HEX_DRAIN_CURSE,
+	HEX_VAMP_BLADE,
+	HEX_STUN_CREATURES,
+	HEX_SHADOW_MOVE,
+	HEX_ANTI_MAGIC,
+	HEX_REVENGE,
+};
 
 
 /*
