d64cefeba7e30894fdae873f2001fb320ecc4c3a
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-07-17 02:09:44 +0000

    Fix editor command definition.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@3736 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/autopick.c
+++ source/trunk/src/autopick.c
@@ -3861,93 +3861,103 @@ static void search_for_string(text_body_type *tb, cptr search_str, bool forward)
 }
 
 
-
-
 /*
  * Editor command id's
  */
-#define EC_QUIT                1 
-#define EC_SAVEQUIT            2     
-#define EC_REVERT	       3 
-#define EC_HELP                4 
-#define EC_RETURN	       5        
-#define EC_LEFT		       6 
-#define EC_DOWN		       7 
-#define EC_UP		       8 
-#define EC_RIGHT	       9 
-#define EC_BOL		       10
-#define EC_EOL		       11
-#define EC_PGUP		       12
-#define EC_PGDOWN	       13
-#define EC_TOP		       14
-#define EC_BOTTOM	       15
-#define EC_CUT		       16
-#define EC_COPY		       17
-#define EC_PASTE	       18
-#define EC_BLOCK	       19
-#define EC_KILL_LINE	       20
-#define EC_DELETE_CHAR	       21
-#define EC_BACKSPACE	       22
-#define EC_SEARCH_STR	       23
-#define EC_SEARCH_FORW         24
-#define EC_SEARCH_BACK         25
-#define EC_SEARCH_OBJ	       26
-#define EC_SEARCH_DESTROYED    27
-#define EC_INSERT_OBJECT       28
-#define EC_INSERT_DESTROYED    29
-#define EC_INSERT_BLOCK	       30
-#define EC_INSERT_MACRO	       31
-#define EC_INSERT_KEYMAP       32
-#define EC_CL_AUTOPICK	       33
-#define EC_CL_DESTROY	       34
-#define EC_CL_LEAVE	       35
-#define EC_CL_QUERY	       36
-#define EC_CL_NO_DISP	       37
-#define EC_OK_COLLECTING       38
-#define EC_IK_UNAWARE	       39
-#define EC_IK_UNIDENTIFIED     40
-#define EC_IK_IDENTIFIED       41
-#define EC_IK_STAR_IDENTIFIED  42
-#define EC_OK_BOOSTED	       43
-#define EC_OK_MORE_DICE	       44
-#define EC_OK_MORE_BONUS       45
-#define EC_OK_WORTHLESS	       46
-#define EC_OK_ARTIFACT	       47
-#define EC_OK_EGO	       48
-#define EC_OK_GOOD	       49
-#define EC_OK_NAMELESS	       50
-#define EC_OK_AVERAGE	       51
-#define EC_OK_RARE	       52       
-#define EC_OK_COMMON	       53
-#define EC_OK_WANTED	       54
-#define EC_OK_UNIQUE	       55
-#define EC_OK_HUMAN	       56
-#define EC_OK_UNREADABLE       57
-#define EC_OK_REALM1	       58
-#define EC_OK_REALM2	       59
-#define EC_OK_FIRST	       60
-#define EC_OK_SECOND	       61
-#define EC_OK_THIRD	       62
-#define EC_OK_FOURTH	       63
-#define EC_KK_WEAPONS	       64
-#define EC_KK_FAVORITE_WEAPONS 65
-#define EC_KK_ARMORS	       66
-#define EC_KK_MISSILES	       67
-#define EC_KK_DEVICES	       68
-#define EC_KK_LIGHTS	       69
-#define EC_KK_JUNKS	       70
-#define EC_KK_CORPSES	       71
-#define EC_KK_SPELLBOOKS       72
-#define EC_KK_SHIELDS	       73
-#define EC_KK_BOWS	       74
-#define EC_KK_RINGS	       75
-#define EC_KK_AMULETS	       76
-#define EC_KK_SUITS	       77
-#define EC_KK_CLOAKS	       78
-#define EC_KK_HELMS	       79
-#define EC_KK_GLOVES	       80
-#define EC_KK_BOOTS	       81
+enum EDITOR_COMMAND
+{
+	EC_NONE,
 
+	EC_QUIT,
+	EC_SAVEQUIT,
+	EC_REVERT,
+	EC_HELP,
+	EC_RETURN,
+	EC_LEFT,
+	EC_DOWN,
+	EC_UP,
+	EC_RIGHT,
+	EC_BOL,
+
+	EC_EOL,
+	EC_PGUP,
+	EC_PGDOWN,
+	EC_TOP,
+	EC_BOTTOM,
+	EC_CUT,
+	EC_COPY,
+	EC_PASTE,
+	EC_BLOCK,
+	EC_KILL_LINE,
+
+	EC_DELETE_CHAR,
+	EC_BACKSPACE,
+	EC_SEARCH_STR,
+	EC_SEARCH_FORW,
+	EC_SEARCH_BACK,
+	EC_SEARCH_OBJ,
+	EC_SEARCH_DESTROYED,
+	EC_INSERT_OBJECT,
+	EC_INSERT_DESTROYED,
+	EC_INSERT_BLOCK,
+
+	EC_INSERT_MACRO,
+	EC_INSERT_KEYMAP,
+	EC_CL_AUTOPICK,
+	EC_CL_DESTROY,
+	EC_CL_LEAVE,
+	EC_CL_QUERY,
+	EC_CL_NO_DISP,
+	EC_OK_COLLECTING,
+	EC_IK_UNAWARE,
+	EC_IK_UNIDENTIFIED,
+
+	EC_IK_IDENTIFIED,
+	EC_IK_STAR_IDENTIFIED,
+	EC_OK_BOOSTED,
+	EC_OK_MORE_DICE,
+	EC_OK_MORE_BONUS,
+	EC_OK_WORTHLESS,
+	EC_OK_ARTIFACT,
+	EC_OK_EGO,
+	EC_OK_GOOD,
+	EC_OK_NAMELESS,
+
+	EC_OK_AVERAGE,
+	EC_OK_RARE,
+	EC_OK_COMMON,
+	EC_OK_WANTED,
+	EC_OK_UNIQUE,
+	EC_OK_HUMAN,
+	EC_OK_UNREADABLE,
+	EC_OK_REALM1,
+	EC_OK_REALM2,
+	EC_OK_FIRST,
+
+	EC_OK_SECOND,
+	EC_OK_THIRD,
+	EC_OK_FOURTH,
+	EC_KK_WEAPONS,
+	EC_KK_FAVORITE_WEAPONS,
+	EC_KK_ARMORS,
+	EC_KK_MISSILES,
+	EC_KK_DEVICES,
+	EC_KK_LIGHTS,
+	EC_KK_JUNKS,
+
+	EC_KK_CORPSES,
+	EC_KK_SPELLBOOKS,
+	EC_KK_SHIELDS,
+	EC_KK_BOWS,
+	EC_KK_RINGS,
+	EC_KK_AMULETS,
+	EC_KK_SUITS,
+	EC_KK_CLOAKS,
+	EC_KK_HELMS,
+	EC_KK_GLOVES,
+
+	EC_KK_BOOTS,
+};
 
 /* Manu names */
 #ifdef JP
