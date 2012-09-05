bc5548874325911eddc3e8f8c993ccad74e97635
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-09-05 22:19:18 +0000

    remove get_species_num_prep3().
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@4326 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/creature_control.c
+++ source/trunk/src/creature_control.c
@@ -1495,62 +1495,6 @@ errr get_species_num_prep(creature_type *summoner_ptr, creature_hook_type creatu
 	return (0);
 }
 
-
-// Apply a "creature restriction function" to the "creature allocation table"
-errr get_species_num_prep3(creature_type *summoner_ptr, creature_hook_type creature_hook, creature_hook_type2 creature_hook2)
-{
-	floor_type *floor_ptr = GET_FLOOR_PTR(summoner_ptr);
-	int i;
-
-	/* Todo: Check the hooks for non-changes */
-
-	/* Set the new hooks */
-	creature_hook_type  get_species_num_hook  = creature_hook;
-	creature_hook_type2 get_species_num2_hook = creature_hook2;
-
-	/* Scan the allocation table */
-	for (i = 0; i < alloc_species_size; i++)
-	{
-		species_type	*r_ptr;
-		
-		/* Get the entry */
-		alloc_entry *entry = &alloc_species_table[i];
-
-		entry->prob2 = 0;
-		r_ptr = &species_info[entry->index];
-
-		/* Skip creatures which don't pass the restriction */
-		if ((get_species_num_hook  && !((*get_species_num_hook) (entry->index))) ||
-		    (get_species_num2_hook && !((*get_species_num2_hook)(summoner_ptr, entry->index))))
-			continue;
-
-		if (!floor_ptr->gamble_arena_mode && !chameleon_change_m_idx &&
-		    summon_specific_type != SUMMON_GUARDIANS)
-		{
-			/* Hack -- don't create questors */
-			if (has_trait_species(r_ptr, TRAIT_QUESTOR)) continue;
-			if (has_trait_species(r_ptr, TRAIT_GUARDIAN)) continue;
-
-			/* Depth Creatures never appear out of depth */
-			if (has_trait_species(r_ptr, TRAIT_FORCE_DEPTH) && (r_ptr->level > floor_ptr->floor_level)) continue;
-		}
-
-		/* Accept this creature */
-		entry->prob2 = entry->prob1;
-
-		//TODO if (floor_ptr->floor_level && (!inside_quest || is_fixed_quest_idx(inside_quest)) && !restrict_creature_to_dungeon(entry->index) && !floor_ptr->gamble_arena_mode)
-		{
-			int hoge = entry->prob2 * dungeon_info[floor_ptr->dun_type].special_div;
-			entry->prob2 = hoge / 64;
-			if (randint0(64) < (hoge & 0x3f)) entry->prob2++;
-		}
-	}
-
-	/* Success */
-	return (0);
-}
-
-
 static int mysqrt(int n)
 {
 	int tmp = n>>1;
@@ -4417,7 +4361,7 @@ bool place_creature_species(creature_type *summoner_ptr, floor_type *floor_ptr, 
 			if (!cave_empty_bold2(floor_ptr, ny, nx)) continue;
 
 			/* Prepare allocation table */
-			get_species_num_prep3(summoner_ptr, get_creature_hook2(ny, nx), place_creature_okay); // TODO
+			get_species_num_prep(summoner_ptr, NULL, get_creature_hook2(ny, nx), place_creature_okay); // TODO
 			if(place_creature_one(summoner_ptr, floor_ptr, ny, nx, m_ptr->underling_id[i], MONEGO_NORMAL, mode) == max_creature_idx);
 				n++;
 		}
@@ -4456,7 +4400,7 @@ bool place_creature_species(creature_type *summoner_ptr, floor_type *floor_ptr, 
 			if (!cave_empty_bold2(floor_ptr, ny, nx)) continue;
 
 			/* Prepare allocation table */
-			get_species_num_prep3(summoner_ptr, get_creature_hook2(ny, nx), place_creature_okay); // TODO
+			get_species_num_prep(summoner_ptr, NULL, get_creature_hook2(ny, nx), place_creature_okay); // TODO
 
 			/* Pick a random race */
 			z = get_species_num(floor_ptr, r_ptr->level);
@@ -4747,7 +4691,7 @@ bool summon_specific(creature_type *summoner_ptr, int y1, int x1, int lev, int t
 	summon_unique_okay = (mode & PM_ALLOW_UNIQUE) ? TRUE : FALSE;
 
 	/* Prepare allocation table */
-	get_species_num_prep3(summoner_ptr, get_creature_hook2(y, x), summon_specific_okay);
+	get_species_num_prep(summoner_ptr, NULL, get_creature_hook2(y, x), summon_specific_okay);
 
 	/* Pick a creature, using the level calculation */
 	species_idx = get_species_num(floor_ptr, (floor_ptr->floor_level + lev) / 2 + 5);
