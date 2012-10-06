a510d1b455d4de4c2044b7613be81dde2ff516fa
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-10-06 07:14:04 +0000

    Merging TRAIT_BR_WALL process in do_active_trait().
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@4784 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/trait.c
+++ source/trunk/src/trait.c
@@ -1597,14 +1597,14 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 
 	case TRAIT_BR_WALL:
 		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
-#ifdef JP
-		else msg_print("フォースのブレスを吐いた。");
-#else
-		else msg_print("You breathe force.");
-#endif
-
 		cast_ball(caster_ptr, GF_FORCE, dir, damage, (user_level > 35 ? -3 : -2));
 		break;
+		{
+			damage = ((caster_ptr->chp / 6) > 200 ? 200 : (caster_ptr->chp / 6));
+			breath(y, x, caster_ptr, GF_FORCE, damage,0, TRUE, TRAIT_BR_WALL, learnable);
+			break;
+		}
+
 	case TRAIT_BR_MANA:
 		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
 #ifdef JP
@@ -3715,25 +3715,6 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 
 
 
-	case TRAIT_BR_WALL:
-		{
-
-#ifdef JP
-			if(blind) msg_format("%^sが何かのブレスを吐いた。", caster_name);
-#else
-			if(blind) msg_format("%^s breathes.", caster_name);
-#endif
-
-#ifdef JP
-			else msg_format("%^sがフォースのブレスを吐いた。", caster_name);
-#else
-			else msg_format("%^s breathes force.", caster_name);
-#endif
-
-			damage = ((caster_ptr->chp / 6) > 200 ? 200 : (caster_ptr->chp / 6));
-			breath(y, x, caster_ptr, GF_FORCE, damage,0, TRUE, TRAIT_BR_WALL, learnable);
-			break;
-		}
 
 	case TRAIT_BR_MANA:
 		{
@@ -5237,25 +5218,6 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 
 
 
-	case TRAIT_BR_WALL:
-		{
-
-#ifdef JP
-			if(blind) msg_format("%^sが何かのブレスを吐いた。", target_name);
-#else
-			if(blind) msg_format("%^s breathes.", target_name);
-#endif
-
-#ifdef JP
-			else msg_format("%^sがフォースのブレスを吐いた。", target_name);
-#else
-			else msg_format("%^s breathes force.", target_name);
-#endif
-
-			damage = ((caster_ptr->chp / 6) > 200 ? 200 : (caster_ptr->chp / 6));
-			breath(y, x, caster_ptr, GF_FORCE, damage,0, TRUE, TRAIT_BR_WALL, learnable);
-			break;
-		}
 
 	case TRAIT_BR_MANA:
 		{
@@ -6804,16 +6766,6 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 			break;
 		}
 
-	case TRAIT_BR_WALL:
-		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
-#ifdef JP
-		else msg_print("フォースのブレスを吐いた。");
-#else
-		else msg_print("You breathe force.");
-#endif
-		damage = MIN(hp / 6, 200);
-		cast_ball(caster_ptr, GF_FORCE, dir, damage, (user_level > 40 ? -3 : -2));
-		break;
 	case TRAIT_BR_MANA:
 		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
 #ifdef JP
