d720bf0d5519e9d145bb9197738f91dbfd377e72
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-10-06 07:26:20 +0000

    Merging TRAIT_BR_DISI process in do_active_trait().
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@4786 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/trait.c
+++ source/trunk/src/trait.c
@@ -1635,6 +1635,7 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 
 		cast_ball(caster_ptr, GF_NUKE, dir, damage, (user_level > 35 ? -3 : -2));
 		break;
+
 	case TRAIT_BA_CHAO:
 		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
 #ifdef JP
@@ -1645,16 +1646,17 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 
 		cast_ball(caster_ptr, GF_CHAOS, dir, damage, 4);
 		break;
+
 	case TRAIT_BR_DISI:
 		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
-#ifdef JP
-		else msg_print("分解のブレスを吐いた。");
-#else
-		else msg_print("You breathe disintegration.");
-#endif
-
 		cast_ball(caster_ptr, GF_DISINTEGRATE, dir, damage, (user_level > 35 ? -3 : -2));
 		break;
+		{
+			damage = ((caster_ptr->chp / 6) > 150 ? 150 : (caster_ptr->chp / 6));
+			breath(y, x, caster_ptr, GF_DISINTEGRATE, damage,0, TRUE, TRAIT_BR_DISI, learnable);
+			break;
+		}
+
 	case TRAIT_BA_ACID:
 		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
 #ifdef JP
@@ -3771,25 +3773,6 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 			break;
 		}
 
-	case TRAIT_BR_DISI:
-		{
-
-#ifdef JP
-			if(blind) msg_format("%^sが何かのブレスを吐いた。", caster_name);
-#else
-			if(blind) msg_format("%^s breathes.", caster_name);
-#endif
-
-#ifdef JP
-			else msg_format("%^sが分解のブレスを吐いた。", caster_name);
-#else
-			else msg_format("%^s breathes disintegration.", caster_name);
-#endif
-
-			damage = ((caster_ptr->chp / 6) > 150 ? 150 : (caster_ptr->chp / 6));
-			breath(y, x, caster_ptr, GF_DISINTEGRATE, damage,0, TRUE, TRAIT_BR_DISI, learnable);
-			break;
-		}
 
 
 	case TRAIT_BA_ACID:
@@ -5252,26 +5235,6 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 			damage = (has_trait(caster_ptr, TRAIT_POWERFUL) ? (user_level * 3) : (user_level * 2))+ diceroll(10, 10);
 			breath(y, x, caster_ptr, GF_CHAOS, damage, 4, FALSE, TRAIT_BA_CHAO, learnable);
 			update_smart_learn(caster_ptr, DRS_CHAOS);
-			break;
-		}
-
-	case TRAIT_BR_DISI:
-		{
-
-#ifdef JP
-			if(blind) msg_format("%^sが何かのブレスを吐いた。", target_name);
-#else
-			if(blind) msg_format("%^s breathes.", target_name);
-#endif
-
-#ifdef JP
-			else msg_format("%^sが分解のブレスを吐いた。", target_name);
-#else
-			else msg_format("%^s breathes disintegration.", target_name);
-#endif
-
-			damage = ((caster_ptr->chp / 6) > 150 ? 150 : (caster_ptr->chp / 6));
-			breath(y, x, caster_ptr, GF_DISINTEGRATE, damage,0, TRUE, TRAIT_BR_DISI, learnable);
 			break;
 		}
 
@@ -6749,16 +6712,6 @@ bool do_active_trait(creature_type *caster_ptr, int id)
 #endif
 		damage = user_level * 4 + diceroll(10, 10);
 		cast_ball(caster_ptr, GF_CHAOS, dir, damage, 4);
-		break;
-	case TRAIT_BR_DISI:
-		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
-#ifdef JP
-		else msg_print("分解のブレスを吐いた。");
-#else
-		else msg_print("You breathe disintegration.");
-#endif
-		damage = MIN(hp / 6, 150);
-		cast_ball(caster_ptr, GF_DISINTEGRATE, dir, damage, (user_level > 40 ? -3 : -2));
 		break;
 	case TRAIT_BA_ACID:
 		if(!get_aim_dir(caster_ptr, &dir)) return FALSE;
