7b68d84808ba650681d9046cd684013dc5b634a8
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-10-30 03:49:03 +0000

    Fix mutation.c.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@5156 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/mutation.c
+++ source/trunk/src/mutation.c
@@ -83,15 +83,11 @@ int count_bits(u32b x)
 {
 	int n = 0;
 
-	if(x) do
-	{
-		n++;
-	}
-	while (0 != (x = x&(x-1)));
+	if(x) do n++;
+	while (0 != (x = x & (x - 1)));
 
 	return (n);
 }
-
 
 static int count_mutations(creature_type *creature_ptr)
 {
@@ -99,36 +95,26 @@ static int count_mutations(creature_type *creature_ptr)
 	return 0;
 }
 
-
-/*
- * Return the modifier to the regeneration rate
- * (in percent)
- */
+// Return the modifier to the regeneration rate
+// (in percent)
 int calc_regenerate_mod(creature_type *creature_ptr)
 {
-	int regen;
-	int mod = 10;
+	int regen, mod;
 	int count = count_mutations(creature_ptr);
 
-	/*
-	 * Beastman get 10 "free" mutations and
-	 * only 5% decrease per additional mutation
-	 */
-
-	if(creature_ptr->chara_idx == CHARA_LUCKY) count--;
+	// Kaleidoscopic race get 10 "free" mutations and
+	// only 5% decrease per additional mutation
 	if(has_trait(creature_ptr, TRAIT_KALEIDOSCOPIC_RACE))
 	{
 		count -= 10;
 		mod = 5;
 	}
+	else mod = 10;
 
-	/* No negative modifier */
-	if(count <= 0) return 100;
+	if(count <= 0) return 100;	// No negative modifier
 
 	regen = 100 - count * mod;
-
-	/* Max. 90% decrease in regeneration speed */
-	if(regen < 10) regen = 10;
+	if(regen < 10) regen = 10;	// Max. 90% decrease in regeneration speed
 
 	return (regen);
 }
