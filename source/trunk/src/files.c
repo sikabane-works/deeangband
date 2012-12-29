bd8493385383cc05f352c91d8f542dc4831eab14
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2012-12-29 04:34:52 +0000

    Fix display_creature_trait().
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@6008 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/files.c
+++ source/trunk/src/files.c
@@ -2897,8 +2897,12 @@ static void display_creature_trait(creature_type *creature_ptr)
 
 	for(i = 0; i < MAX_TRAITS; i++)
 	{
-		if(has_trait(creature_ptr, i)) c_put_str(TERM_WHITE, format("E%s", trait_info[i].title), n + 1, 1);
-		n++;
+		if(has_trait(creature_ptr, i))
+		{
+			c_put_str(TERM_WHITE, format("E%s", trait_info[i].title), n + 2, 1);
+			n++;
+		}
+		if(n > 10) break;
 	}
 
 	if(n) c_put_str(TERM_YELLOW, SYS_MESAAGE_YOUR_TRAITS, 1, 1);
