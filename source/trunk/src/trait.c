f19ec2d4f10aa1d51beed143ea05cbadc15c5183
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-07-04 04:15:48 +0000

    Add trait, TRAIT_RUMOR.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@8455 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/trait.c
+++ source/trunk/src/trait.c
@@ -2181,6 +2181,42 @@ bool do_active_trait(creature_type *caster_ptr, int id, bool message)
 		(void)restore_exp(caster_ptr);
 		break;
 
+	case TRAIT_RUMOR:
+		{
+			char Rumor[MAX_NLEN];
+			errr err = 0;
+			switch (randint1(20))
+			{
+
+				case 1:
+					err = get_rnd_line(TEXT_FILES_CHAINSWORD, 0, Rumor);
+					break;
+
+				case 2:
+					err = get_rnd_line(TEXT_FILES_ERROR, 0, Rumor);
+					break;
+				case 3:
+				case 4:
+				case 5:
+					err = get_rnd_line(TEXT_FILES_DEATH, 0, Rumor);
+					break;
+
+				default:
+					err = get_rnd_line(TEXT_FILES_RUMOR, 0, Rumor);
+					break;
+			}
+
+			/* An error occured */
+			if(err) strcpy(Rumor, MES_SCROLL_RUMOR_DEFAULT);
+			msg_print(MES_SCROLL_MESSAGE);
+			msg_print(NULL);
+			msg_format("%s", Rumor);
+			msg_print(NULL);
+			msg_print(MES_SCROLL_USED);
+			break;
+		}
+		break;
+
 	case 3: /* TRAIT_LAUNCHER */
 		/* Gives a multiplier of 2 at first, up to 3 at 40th */
 		if(!do_cmd_throw_aux(caster_ptr, 2 + user_level / 40, FALSE, 0)) return FALSE;
