1aa9a9e7f8d954914c19436575c83a5c62a45045
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-07-16 15:36:25 +0000

    Merging scroll effect to active trait.(RUMOR). (http://sourceforge.jp/ticket/browse.php?group_id=5614&tid=31458)
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@8651 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/trunk/src/cmd6.c
+++ source/trunk/src/cmd6.c
@@ -705,40 +705,6 @@ static void do_cmd_read_scroll_aux(creature_type *caster_ptr, int item)
 			ident = TRUE;
 			break;
 
-		case SV_SCROLL_RUMOR:
-		{
-			errr err = 0;
-			switch (randint1(20))
-			{
-				case 1:
-					err = get_rnd_line(TEXT_FILES_CHAINSWORD, 0, Rumor);
-					break;
-
-				case 2:
-					err = get_rnd_line(TEXT_FILES_ERROR, 0, Rumor);
-					break;
-				case 3:
-				case 4:
-				case 5:
-					err = get_rnd_line(TEXT_FILES_DEATH, 0, Rumor);
-					break;
-
-				default:
-					err = get_rnd_line(TEXT_FILES_RUMOR, 0, Rumor);
-					break;
-			}
-
-			/* An error occured */
-			if(err) strcpy(Rumor, MES_SCROLL_RUMOR_DEFAULT);
-			msg_print(MES_SCROLL_MESSAGE);
-			msg_print(NULL);
-			msg_format("%s", Rumor);
-			msg_print(NULL);
-			msg_print(MES_SCROLL_USED);
-			ident = TRUE;
-			break;
-		}
-
 		case SV_SCROLL_ARTIFACT:
 		{
 			ident = TRUE;
