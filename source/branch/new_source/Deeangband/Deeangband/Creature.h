7809927876b4db4b1c8d193b369650febd009752
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-10 16:25:01 +0000

    Constructing of Creature class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9131 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/branch/new_source/Deeangband/Deeangband/Creature.h
+++ source/branch/new_source/Deeangband/Deeangband/Creature.h
@@ -9,7 +9,10 @@
 #include "stdafx.h"
 #include "GameElement.h"
 
-typedef int Status;
+typedef int Status; /*!<基礎能力値*/
+typedef int HP; /*!<HP*/
+typedef int MP; /*!<MP*/
+typedef int POWER; /*!<効力*/
 
 /*! @enum CREATURE_STATUS
     @brief クリーチャーの基礎能力値6種の列挙体
@@ -42,18 +45,37 @@ struct status_table
 class Creature : public GameElement
 {
 private:
+
 	string name;
-	int current_hp; /*!< 現在のHP*/
-	int max_hp; /*!< 現在の最大HP*/
-	int max_max_hp; /*!< 本来の最大HP*/
-	int current_mp; /*!< 現在のHP*/
-	int max_mp; /*!< 現在の最大HP*/
-	int max_max_mp; /*!< 本来の最大HP*/
+	HP current_hp; /*!< 現在のHP*/
+	HP max_hp; /*!< 現在の最大HP*/
+	HP max_max_hp; /*!< 本来の最大HP*/
+	MP current_mp; /*!< 現在のHP*/
+	MP max_mp; /*!< 現在の最大HP*/
+	MP max_max_mp; /*!< 本来の最大HP*/
+
+	/*! クリーチャー死亡時の処理を行う */
+	void dead(void);
+
+	/*! クリーチャーの現HPを増減させる */
+	void calc_hp(HP amount);
+
+	/*! クリーチャーの現MPを増減させる */
+	void calc_mp(MP amount);
+
+	/*! クリーチャーの現HPを設定する */
+	void set_hp(HP amount);
+
+	/*! クリーチャーの現MPを設定する */
+	void set_mp(MP amount);
 
 public:
 
-	/*!< クリーチャーが生存している状態かを返す */
+	/*! クリーチャーが生存している状態かを返す */
 	bool is_alive(void);
-	
+
+	/*! クリーチャーに属性効果を与える */
+	bool take_effect(POWER amount);
+
 };
 
