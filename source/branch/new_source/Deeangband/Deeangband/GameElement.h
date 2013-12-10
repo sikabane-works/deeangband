7d65a7a43dbd53f609c46505bcca4bd1608c2c6f
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-10 16:46:10 +0000

    Add comment for Doxygen to GameElement class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9133 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/branch/new_source/Deeangband/Deeangband/GameElement.h
+++ source/branch/new_source/Deeangband/Deeangband/GameElement.h
@@ -1,9 +1,21 @@
-typedef int ID;
+/*!
+    @file GameElement.h
+    @brief GameElementクラスとその他付随要素のプロトタイプ
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
 
+typedef int ID; /*!<ゲーム要素ID*/
+
+/*! @class GameElement
+    @brief ゲームの諸要素の管理を行うスーパークラス
+*/
 class GameElement
 {
 private:
-	ID id;
+	ID id; /*!<ゲーム要素のID*/
 public:
-	ID getid();
+	void set_id(); /*!<IDを設定する*/
+	ID get_id();   /*!<IDを取得する*/
 };
