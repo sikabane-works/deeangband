401b34a9c0f62f5037cdae39eca67da82387517d
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-11 14:53:22 +0000

    Add Dungeon class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9136 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Dungeon.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file Dungeon.cpp
+    @brief ゲーム中のクリーチャーを定義する
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "Dungeon.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/Dungeon.h
@@ -0,0 +1,19 @@
+/*!
+    @file Dungeon.h
+    @brief ゲーム中のダンジョンと付随要素のプロトタイプ
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class Dungeon
+    @brief ダンジョンのクラス
+*/
+class Dungeon : public GameElement
+{
+private:
+public:
+};
