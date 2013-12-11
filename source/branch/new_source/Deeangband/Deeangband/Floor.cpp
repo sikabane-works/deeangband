c1086a21987937e8f854dae3b433d1505734ec73
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-11 14:56:25 +0000

    Add Floor class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9137 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Floor.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file Floor.cpp
+    @brief ゲーム中のフロアを定義する
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "Floor.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/Floor.h
@@ -0,0 +1,19 @@
+/*!
+    @file Floor.h
+    @brief ゲーム中のダンジョンと付随要素のプロトタイプ
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class Floor
+    @brief フロアのクラス
+*/
+class Floor : public GameElement
+{
+private:
+public:
+};
