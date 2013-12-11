ef1ee81b1f60ac56b67b3609af768d7393aae47c
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-11 14:47:47 +0000

    Implementing Creature class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9135 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/branch/new_source/Deeangband/Deeangband/Creature.cpp
+++ source/branch/new_source/Deeangband/Deeangband/Creature.cpp
@@ -13,3 +13,40 @@ bool Creature::is_alive(void)
 {
 	return true;
 }
+
+bool Creature::take_effect(POWER amount)
+{
+	return false;
+}
+
+void Creature::dead(void)
+{
+}
+
+void Creature::calc_hp(HP amount)
+{
+	this->current_hp += amount;
+	if(this->current_hp > this->max_hp) this->current_hp = this->max_hp;
+	if(this->current_hp < 1) this->dead();
+}
+
+void Creature::calc_mp(MP amount)
+{
+	this->current_mp += amount;
+	if(this->current_mp > this->max_mp) this->current_mp = this->max_mp;
+	if(this->current_mp < 0) this->current_mp = 0;
+}
+
+void Creature::set_hp(HP amount)
+{
+	this->current_hp = amount;
+	if(this->current_hp < this->max_hp) this->current_hp = this->max_hp;
+	if(this->current_hp < 1) this->dead();
+}
+
+void Creature::set_mp(MP amount)
+{
+	this->current_mp += amount;
+	if(this->current_mp > this->max_mp) this->current_mp = this->max_mp;
+	if(this->current_mp < 0) this->current_mp = 0;
+}
