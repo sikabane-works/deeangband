890cddca56b8c1bfe42bb49aa85b9c9cf644839d
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-11 14:59:02 +0000

    Add Cave class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9138 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Cave.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file Cave.cpp
+    @brief ゲーム中のマスを定義する
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "Cave.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/Cave.h
@@ -0,0 +1,19 @@
+/*!
+    @file Cave.h
+    @brief ゲーム中のマスと付随要素のプロトタイプ
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class Cave
+    @brief フロアのクラス
+*/
+class Cave : public GameElement
+{
+private:
+public:
+};
