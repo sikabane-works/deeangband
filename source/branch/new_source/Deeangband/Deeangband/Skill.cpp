9ac9a319877e6966f95eb3bcaa52e60d52ace5b5
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-17 12:34:44 +0000

    Add Skill class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9145 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Skill.cpp
@@ -0,0 +1,11 @@
+/*!
+    @file Skill.cpp
+    @brief ゲーム中のスキルを定義する
+    @date 2013/12/17
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "Skill.h"
+
---
+++ source/branch/new_source/Deeangband/Deeangband/Skill.h
@@ -0,0 +1,19 @@
+/*!
+    @file Skill.h
+    @brief ゲーム中のスキルのプロトタイプ
+    @date 2013/12/17
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class Skill
+    @brief スキルのクラス
+*/
+class Skill : public GameElement
+{
+private:
+public:
+};
