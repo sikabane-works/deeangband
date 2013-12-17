53d049e86f53e9c03ab519a91cec104f90ff7010
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-17 12:13:40 +0000

    Add ItemEgo class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9143 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/ItemEgo.h
@@ -0,0 +1,21 @@
+/*!
+    @file ItemEfo.h
+    @brief ゲーム中のアイテムエゴのプロトタイプ
+    @date 2013/12/17
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class ItemEgo
+    @brief アイテムエゴのクラス
+*/
+class ItemEgo : public GameElement
+{
+
+private:
+public:
+
+};
