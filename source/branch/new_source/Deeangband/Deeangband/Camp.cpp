1bc421d69f82bcf9456916bb90ab93a425aabef1
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-19 14:50:15 +0000

    Add Camp class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9146 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Camp.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file Creature.cpp
+    @brief ƒQ[ƒ€’†‚Ìw‰c‚ğ’è‹`‚·‚é
+    @date 2013/12/19
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "Camp.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/Camp.h
@@ -0,0 +1,19 @@
+/*!
+    @file Creature.h
+    @brief ゲーム中の陣営と付随要素のプロトタイプ
+    @date 2013/12/11
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+class Creature : public GameElement
+{
+
+private:
+
+public:
+
+};
