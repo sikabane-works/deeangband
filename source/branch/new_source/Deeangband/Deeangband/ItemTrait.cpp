9e9075c412b8d603c19ee4707602692752d8e559
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-22 12:42:37 +0000

    Add ItemTrait class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9150 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/ItemTrait.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file ItemTrait.cpp
+    @brief ゲーム中のアイテム特性を定義する
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "ItemTrait.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/ItemTrait.h
@@ -0,0 +1,19 @@
+/*!
+    @file ItemTrait.h
+    @brief ゲーム中のアイテム特性のプロトタイプ定義
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class ItemTrait
+    @brief アイテム特性のクラス
+*/
+class ItemTrait : public GameElement
+{
+private:
+public:
+};
