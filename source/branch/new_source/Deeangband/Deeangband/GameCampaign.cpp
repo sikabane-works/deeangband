694ed16ac54df1ff724f5c8fe12052926b635429
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-22 13:27:00 +0000

    Add Effect class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9155 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/GameCampaign.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file GameCampaign.cpp
+    @brief ƒQ[ƒ€¢ŠE‚ğ’è‹`‚·‚é
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameCampaign.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/GameCampaign.h
@@ -0,0 +1,20 @@
+/*!
+    @file GameCampaign.h
+    @brief ゲームキャンペーンのプロトタイプ宣言
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#pragma once
+
+#include "stdafx.h"
+
+/*! @class GameCampaignのクラス
+    @brief ゲームキャンペーンのクラス
+*/
+class GameCampaign
+{
+private:
+public:
+};
