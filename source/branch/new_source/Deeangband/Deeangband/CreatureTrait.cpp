248889d8d8a568336266753771b3f64e09f23877
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-22 12:37:47 +0000

    Add CreatureTrait class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9149 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/CreatureTrait.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file CreatureTriat.cpp
+    @brief ゲーム中のクリーチャー特性を定義する
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "CreatureTrait.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/CreatureTrait.h
@@ -0,0 +1,19 @@
+/*!
+    @file CreatureTrait.h
+    @brief ゲーム中のクリーチャー特性のプロトタイプ
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class CreatureTrait
+    @brief クリーチャー特性のクラス
+*/
+class CreatureTrait : public GameElement
+{
+private:
+public:
+};
