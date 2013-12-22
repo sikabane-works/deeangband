23a65260a6378d8c779ce9f6da3f015e889ee0f6
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2013-12-22 13:28:53 +0000

    Implementing relationship between Classes.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9156 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Effect.cpp
@@ -0,0 +1,10 @@
+/*!
+    @file Effect.cpp
+    @brief ƒQ[ƒ€’†‚ÌŒø‰Ê‘®«‚ğ’è‹`‚·‚é
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#include "stdafx.h"
+#include "Effect.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/Effect.h
@@ -0,0 +1,21 @@
+/*!
+    @file Effect.h
+    @brief 効果属性のプロトタイプ宣言
+    @date 2013/12/22
+    @author Deskull
+    2013 Sikabane Works.
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class Effect
+    @brief 効果属性のクラス
+*/
+class Effect : public GameElement
+{
+private:
+public:
+};
