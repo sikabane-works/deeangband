6211d9e40776930b0f87bf6f1aa73b39150f6393
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2014-01-26 05:33:58 +0000

    Implementing Trap class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9173 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Trap.h
@@ -0,0 +1,19 @@
+/*!
+ * @file Trap.h
+ * @brief ゲーム中のトラップのプロトタイプ
+ * @date 2014/01/26
+ * @author Deskull
+ * 2014 Sikabane Works.
+ */
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+class Trap : public GameElement
+{
+
+private:
+
+public:
+
+};
