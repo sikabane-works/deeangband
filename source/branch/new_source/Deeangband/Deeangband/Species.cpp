5cf335900162377a72ae5a1137085a8c6d2fe4eb
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2014-02-01 13:46:53 +0000

    Add Species class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9182 89dd94fa-70ca-40ae-8747-2094edff379b

---
+++ source/branch/new_source/Deeangband/Deeangband/Species.cpp
@@ -0,0 +1,10 @@
+/*!
+ * @file Species.cpp
+ * @brief ゲーム中のクリーチャー種別を定義する
+ * @date 2014/02/01
+ * @author Deskull
+ * 2014 Sikabane Works.
+ */
+
+#include "stdafx.h"
+#include "Species.h"
---
+++ source/branch/new_source/Deeangband/Deeangband/Species.h
@@ -0,0 +1,21 @@
+/*!
+ * @file Species.h
+ * @brief ゲーム中のクリーチャー種別のプロトタイプ
+ * @date 2014/02/01
+ * @author Deskull
+ * 2014 Sikabane Works.
+ */
+
+#include "stdafx.h"
+#include "GameElement.h"
+
+/*! @class Species
+ *  @brief アイテムベースのクラス
+ */
+class Species : public GameElement
+{
+
+private:
+public:
+
+};
