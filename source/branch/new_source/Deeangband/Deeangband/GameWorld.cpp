9b2266ddce80ac066631f6cb01900d123a97e8a3
deskull <deskull@89dd94fa-70ca-40ae-8747-2094edff379b>
2014-02-25 12:57:37 +0000

    Implementing GameWorld class.
    git-svn-id: http://svn.osdn.net/svnroot/deeangband@9237 89dd94fa-70ca-40ae-8747-2094edff379b

--- source/branch/new_source/Deeangband/Deeangband/GameWorld.cpp
+++ source/branch/new_source/Deeangband/Deeangband/GameWorld.cpp
@@ -13,6 +13,27 @@
 GameWorld::GameWorld(GameSurface *gSurface)
 {
 	gameSurface = gSurface;
+
+	authorityList.insert(map<ID, Authority>::value_type(0, Authority()));
+	creatureTraitList.insert(map<ID, CreatureTrait>::value_type(0, CreatureTrait()));
+	effectList.insert(map<ID, Effect>::value_type(0, Effect()));
+	fixedArtifactList.insert(map<ID, FixedArtifact>::value_type(0, FixedArtifact()));
+	featureList.insert(map<ID, Feature>::value_type(0, Feature()));
+	itemBaseList.insert(map<ID, ItemBase>::value_type(0, ItemBase()));
+	itemEgoList.insert(map<ID, ItemEgo>::value_type(0, ItemEgo()));
+	itemTraitList.insert(map<ID, ItemTrait>::value_type(0, ItemTrait()));
+	karmatList.insert(map<ID, Karma>::value_type(0, Karma()));
+	skillList.insert(map<ID, Skill>::value_type(0, Skill()));
+	speciesList.insert(map<ID, Species>::value_type(0, Species()));
+	trapbaseList.insert(map<ID, TrapBase>::value_type(0, TrapBase()));
+
+	buildingList.insert(map<ID, Building>::value_type(0, Building()));
+	campList.insert(map<ID, Camp>::value_type(0, Camp()));
+	creatureList.insert(map<ID, Creature>::value_type(0, Creature()));
+	dungeonList.insert(map<ID, Dungeon>::value_type(0, Dungeon()));
+	itemList.insert(map<ID, Item>::value_type(0, Item()));
+	floorList.insert(map<ID, Floor>::value_type(0, Floor()));
+
 }
 
 GameWorld::~GameWorld(void)
